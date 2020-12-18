b62cbdd2eaa7f9616647d99677ee0a941afad06f
Nicolai Schmid <nicolai@schmid.uno>
2020-12-19 00:36:11 +0100

    feat(Flag): sticky to the top

--- components/Flag.js
+++ components/Flag.js
@@ -1,7 +1,7 @@
 export default function Flag() {
   /* Germany strip */
   return (
-    <div className="w-full h-2 flex justify-evenly">
+    <div className="w-full h-2 flex justify-evenly sticky top-0">
       <div className="w-full h-full bg-black" />
       <div className="w-full h-full bg-red-600" />
       <div className="w-full h-full bg-yellow-400" />
