69311eeb647f8f69808b0d201a1982c56e1eed58
Nicolai Schmid <nicolai@schmid.uno>
2021-02-14 03:30:20 +0100

    feat(Navigate): expand shortcut functionality

--- components/Navigate.js
+++ components/Navigate.js
@@ -2,6 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import { useRouter } from "next/router";
 
+function processEntry(string) {
+  const parts = string.split("§");
+
+  if (parts.length === 1) {
+    return { paragraph: parts[0] };
+  }
+
+  if (parts.length > 2) {
+    console.error("Wrong entry");
+  }
+
+  return { law: parts[0], paragraph: parts[parts.length - 1] };
+}
+
 function NavigateForm({ law, setOpen }) {
   const router = useRouter();
 
@@ -11,7 +25,9 @@ function NavigateForm({ law, setOpen }) {
         onSubmit={(event) => {
           event.preventDefault();
 
-          const newParagraph = event.target.paragraph.value;
+          const { law: newLaw, paragraph: newParagraph } = processEntry(
+            event.target.paragraph.value
+          );
 
           if (!newParagraph) {
             setOpen(false);
@@ -21,14 +37,14 @@ function NavigateForm({ law, setOpen }) {
           setOpen(false);
 
           // route to new paragraph
-          router.push(`/${law}/${newParagraph}`);
+          router.push(`/${newLaw || law}/${newParagraph}`);
         }}
       >
         <input
           type="text"
           id="paragraph"
           name="paragraph"
-          placeholder="Paragraph"
+          placeholder="Shortcut"
           autoFocus
           className="w-full max-auto p-2 mb-2 shadow-lg rounded-lg focus:outline-none focus:ring-2 ring-blue-200 text-gray-800"
         />
