c88de799526bd74635a242cb71ea549a4c6877f3
Nicolai Schmid <nicolai@schmid.uno>
2021-02-14 03:04:18 +0100

    feat(index): add some content to home page

--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,14 @@ import Flag from "./Flag";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-w-screen min-h-screen bg-gray-100">
+    <div className="min-w-screen min-h-screen ">
       <Flag />
       <div className="z-50 relative bg-white shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center py-3 md:justify-start md:space-x-10">
             <div className="w-0 flex-1 flex">
               <Link href="/">
-                <a>Gesetze</a>
+                <a>Gesetze im Internet - Nur angenehmer!</a>
               </Link>
             </div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,55 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <Link href="/hgb/8b">
-        <a>HGB § 8a</a>
-      </Link>
+      <div className="p-8 text-gray-800">
+        <p>
+          Eine angenehme Webapp für{" "}
+          <a
+            href="gesetze-im-internet.de"
+            target="_blank"
+            rel="noopener"
+            className="text-blue-800 underline"
+          >
+            Gesetze im Internet
+          </a>
+          . Die Inhalte sind genau die gleichen, es ist nur einfacher zu lesen
+          und es gibt einige nette Features.
+        </p>
+
+        <h3 className="text-xl mt-4">Bedienung</h3>
+        <p>
+          Alle Gesetze sind über die URL aufrufbar. Man nimmt das Kürzel, dass
+          auch Gesetze-im-Internet benutzt und fügt es an die URL an. Dahinter
+          dann der gewünschte Paragraph, als zweites Pfadsegment. Zum Beispiel
+          das{" "}
+          <Link href="/hgb/1">
+            <a className="text-blue-800 underline">Handelsgesetzbuch, §1</a>
+          </Link>{" "}
+          über{" "}
+          <Link href="/hgb/1">
+            <a className="text-blue-800 underline">"/hgb/1"</a>
+          </Link>
+        </p>
+        <h3 className="text-xl mt-4">
+          Shortcuts (warum es diese Seite überhaupt gibt)
+        </h3>
+        <p>
+          Auf jeder Gesetzestextseite kann mit dem Drücken der Taste "p" eine
+          schnelle Navigation aufgerufen werden, wo der Paragraphname eingegeben
+          werden kann.
+        </p>
+        <br />
+        <p className="">
+          Autor -{" "}
+          <a
+            href="https://nicolaischmid.de"
+            target="_blank"
+            className="text-blue-800 underline"
+          >
+            Nicolai Schmid
+          </a>
+        </p>
+      </div>
     </Layout>
   );
 }
